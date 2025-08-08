d7266f220db483365fc3ed8e13a594494ee237b2
dextar34 <weknownothingbutheknows23@gmail.com>
2025-08-08 12:20:35 +0600

    logo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import HybridSolarCalculator from './components/HybridSolarCalculator'
+import Logo from "/logo.png";
 
 const App = () => {
   return (
     <div>
+      <div className="logo w-72  mx-auto">
+        <a href="https://solaraenergyltd.com/">
+        <img src={Logo} alt="solaraLogo" />
+        </a>
+      </div>
       <HybridSolarCalculator/>
     </div>
   )
